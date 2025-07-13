89a4eeec6d01b38b1e63c81f714bd25d6950d3f1
dingyuqing <dingyuqing@shizhuang-inc.com>
2025-07-13 14:11:23 +0800

    opt log

--- esphome/components/servo/__init__.py
+++ esphome/components/servo/__init__.py
@@ -1,4 +1,4 @@
-import traceback
+# import traceback
 
 from esphome import automation
 from esphome.automation import maybe_simple_id
@@ -66,9 +66,9 @@ async def to_code(config):
     ),
 )
 async def servo_write_to_code(config, action_id, template_arg, args):
-    for item in traceback.format_stack():
-        print(item)
-    assert False
+    # for item in traceback.format_stack():
+    #     print(item)
+    # assert False
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
     template_ = await cg.templatable(config[CONF_LEVEL], args, float)
